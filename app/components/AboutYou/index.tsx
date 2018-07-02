cfdf1d5dab565846a7b39d8cdb88bff780356769
apicker <ari.picker@oracle.com>
2018-07-02 16:03:53 -0400

    Refactor AboutYou to render once

--- app/components/AboutYou/index.tsx
+++ app/components/AboutYou/index.tsx
@@ -2,22 +2,14 @@ import * as React from "react";
 import userStore from "../../stores/userStore";
 import Characters from "../../components/Characters";
 
-interface AboutYouState {
-    dynamicComponent: JSX.Element;
-}
-
-export default class AboutYou extends React.Component<{}, AboutYouState> {
-    state = { dynamicComponent: undefined }
-
-    componentDidMount() {
-        const { id } = userStore.getState().character;
-        this.setState({ dynamicComponent: this.getComponent(id) })
-    }
-
+export default class AboutYou extends React.Component {
     getComponent = name => {
         const ThunderCat = Characters[name].component;
         return <ThunderCat />;
     };
 
-    render() { return <div>{this.state.dynamicComponent}</div>; }
+    render() {
+        const { id } = userStore.getState().character;
+        return <div>{this.getComponent(id)}</div>;
+    }
 }
